62deeba8aed13cb36ce26c8d33624e946ff6bc7f
saladinjake <juwavictor@gmail.com>
2022-06-23 13:16:51 +0100

    add theme

---
+++ src/themes/theme.js
@@ -0,0 +1,22 @@
+export default {
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+  colors: {
+    primary1: "hsl(204,20.8%,85.9%)",
+    background1: "#0f0f1b",
+    accent1: "hsl(34.5,97.6%,62.9%)",
+    button: "hsl(200.4,100%,30.1%)",
+    background2: "#1f1f2b",
+  },
+  
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  
+}
