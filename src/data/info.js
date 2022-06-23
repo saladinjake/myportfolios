c29d3f3c36c67ee65c8121fe69618e42295cb485
saladinjake <juwavictor@gmail.com>
2022-06-23 13:48:08 +0100

    ch: add info data

---
+++ src/data/info.js
@@ -0,0 +1,70 @@
+export const aminbenzData = [
+  {
+    name: 'JUWA VICTOR (SALADINJAKE)',
+    phone: 53710722,
+    email: 'juwavictor@gmail.com',
+    github: 'github.com/saladinjake',
+    instagram: 'github.com/saladinjake',
+    linkedIn: 'https://www.linkedin.com/m/in/victor-dibs-462252b9',
+    portofolioWeb: 'https://victorjuwa.vercel.app',
+    lastProject: 'https://pacemaker.herokuapp.com/',
+    lastProjectName: 'pacemaker',
+    resume: './images/JuwaVictorResume.pdf',
+    age: '25',
+    jobTitle: 'Full stack Developer',
+    summary: {
+      main: 'Full stack Developer a full-time position in the web designing, where i can apply my knowledge and skills for continuous improvement.',
+      secondary:
+        'Full stack Developer with frontend role in Reactjs a full-time position in the web designing, where i can apply my knowledge and skills for continuous improvement,  also  learning Back-End Development and looking forward to be a Full Stack Web Developer.',
+      third:
+        'I am motivated towards  bringing meaningful ideas to life through code. My interest in technology has allowed me to advance my skills with unique projects, providing valuable experience working with others.',
+    },
+    technologys: {
+      frontEnd: ['HTML / CSS / SASS / JAVASCRIPT React/Vue/Angular (ES6) & OOP / Typescript '],
+      backEnd: ['NodeJS / Mongoose / Expressjs / MongoDB / MySQL / POSTGRES/PHP/LARAVEL/DJANGO'],
+      frameWorkes: ['Reactjs / Nextjs / Redux / Angular / Vue / Vuex'],
+      sourseControl: ['Git / GitHub / BitBucket'],
+      
+    },
+  },
+];
+
+export const projects = [
+  {
+    title: 'Commuter',
+    description:
+      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    thumbnail: '/images/1.png',
+    tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Pacemaker',
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics ",
+    thumbnail: '/images/2.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Ecommerce',
+    description:
+      "This is a code repository for the corresponding YouTube video.",
+    thumbnail: '/images/3.jpg',
+    tags: ['React', 'WebRTC'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: 'Worked as a freelance' },
+  { year: 2019, text: 'First time frontend role' },
+  { year: 2020, text: 'Frontend team leader' },
+  { year: 2021, text: 'Worked on personal projects' },
+  { year: 2023, text: 'Started my own platform' },
+];
